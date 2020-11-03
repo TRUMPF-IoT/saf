6ed49673ad79c695177483033189b01b10d0d364
Bernd Brachmaier | AUNOVIS <brachmaier@aunovis.de>
2020-11-03 18:13:23 +0100

    Removed unrequired sources

--- src/Communication/CDMy.SAF.PubSub.Registry/HTML5/ServiceScreen.js
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2017-2020 TRUMPF Laser GmbH
-//
-// SPDX-License-Identifier: MPL-2.0
-
-(function (id) {
-    console.log("Successfully loaded CDEPUBSUB");
-})("%=CONTENT%");
