126de899b809c7e9e8002ea1639e935015f61017
boeschen67 <86786906+boeschen67@users.noreply.github.com>
2021-07-27 13:30:37 +0200

    Update src/Communication/SAF.Communication.PubSub.Cde/SubscriptionRegistry.cs
    Co-authored-by: Bernd Brachmaier | AUNOVIS <50444727+berndbr@users.noreply.github.com>

--- src/Communication/SAF.Communication.PubSub.Cde/SubscriptionRegistry.cs
+++ src/Communication/SAF.Communication.PubSub.Cde/SubscriptionRegistry.cs
@@ -40,7 +40,7 @@ namespace SAF.Communication.PubSub.Cde
     /// Manages the subscriptions of other nodes (each represented by <see cref="RemoteSubscriber"/>).
     /// These will be registered by a subscription event. These subscriptions are used by the parent
     /// <see cref="Publisher"/> to send Messages to all registered subscribers. This class also handle
-    /// Events for example discovery request or subsciber alive request.
+    /// events for example discovery request or subsciber alive request.
     /// </summary>
     internal class SubscriptionRegistry : IDisposable
     {
