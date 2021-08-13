485041f96a9be95cfaccafbb2cf63f4eee55aa3a
Christoph Böschen <Boeschen@aunovis.de>
2021-08-13 08:06:37 +0200

    Licence appended

--- src/Communication/SAF.Communication.PubSub.Cde/ISubscriptionRegistry.cs
+++ src/Communication/SAF.Communication.PubSub.Cde/ISubscriptionRegistry.cs
@@ -1,4 +1,8 @@
-﻿using SAF.Common;
+﻿// SPDX-FileCopyrightText: 2017-2021 TRUMPF Laser GmbH
+//
+// SPDX-License-Identifier: MPL-2.0
+
+using SAF.Common;
 using SAF.Communication.PubSub.Interfaces;
 using System;
 using System.Threading;
