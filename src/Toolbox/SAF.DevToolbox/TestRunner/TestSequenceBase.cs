2907d632c3aefea86b487cd8fc907dfacab04fec
Bernd Brachmaier | AUNOVIS <brachmaier@aunovis.de>
2022-05-19 16:06:41 +0200

    Made TestSequenceBase thread-safe and allow to unsubscribe per dispose pattern

--- src/Toolbox/SAF.DevToolbox/TestRunner/TestSequenceBase.cs
+++ src/Toolbox/SAF.DevToolbox/TestRunner/TestSequenceBase.cs
@@ -1,62 +1,81 @@
-// SPDX-FileCopyrightText: 2017-2020 TRUMPF Laser GmbH
+// SPDX-FileCopyrightText: 2017-2022 TRUMPF Laser GmbH
 //
 // SPDX-License-Identifier: MPL-2.0
 
 using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Threading;
 using SAF.Common;
 
-namespace SAF.DevToolbox.TestRunner
+namespace SAF.DevToolbox.TestRunner;
+
+public abstract class TestSequenceBase : IMessageHandler
 {
-    public abstract class TestSequenceBase : IMessageHandler
+    private readonly IMessagingInfrastructure _messaging;
+    private readonly ConcurrentDictionary<string, Action<string>> _persistActions = new();
+
+    internal Action<string> TraceTitleAction { get; set; }
+    internal Action<string, string> TraceDocumentationAction { get; set; }
+        
+    public bool CanHandle(Message message) => true;
+
+    protected TestSequenceBase(IMessagingInfrastructure messaging)
+    {
+        _messaging = messaging;
+    }
+
+    public void Handle(Message message)
+    {
+        if (!_persistActions.TryGetValue(message.Topic, out var action))
+            return;
+
+        action(message.Payload);
+    }
+
+    protected void WaitForValueSet(ref string value, int timeoutSeconds)
+    {
+        var c = 0;
+        while (value == null && ++c < timeoutSeconds * 5)
+            Thread.Sleep(200);
+
+        if (value == null)
+            throw new TimeoutException();
+    }
+
+    protected IDisposable PayloadToVariable<T>(string topic, Action<string> persistAction) where T : IMessageHandler
+    {
+        _persistActions.AddOrUpdate(topic, persistAction, (key, _) => persistAction);
+        var subscribeId = _messaging.Subscribe<T>(topic);
+        return new DisposableMessagingSubscription(_messaging, subscribeId);
+    }
+
+    protected IDisposable PayloadToVariable(string topic, Action<string> persistAction)
+    {
+        var subscribeId = _messaging.Subscribe(topic, msg => persistAction(msg.Payload));
+        return new DisposableMessagingSubscription(_messaging, subscribeId);
+    }
+
+    protected void TraceDocumentation(string title, string doc) => TraceDocumentationAction?.Invoke(title, doc);
+
+    protected void TraceTitle(string title) => TraceTitleAction?.Invoke(title);
+
+    public abstract void Run();
+
+    private class DisposableMessagingSubscription : IDisposable
     {
         private readonly IMessagingInfrastructure _messaging;
-        private readonly Dictionary<string, Action<string>> _persistActions = new();
+        private readonly object _subscriptionId;
 
-        internal Action<string> TraceTitleAction { get; set; }
-        internal Action<string, string> TraceDocumentationAction { get; set; }
-        
-        public bool CanHandle(Message message) => true;
-
-        protected TestSequenceBase(IMessagingInfrastructure messaging)
+        public DisposableMessagingSubscription(IMessagingInfrastructure messaging, object subscriptionId)
         {
             _messaging = messaging;
+            _subscriptionId = subscriptionId;
         }
 
-        public void Handle(Message message)
+        public void Dispose()
         {
-            if (_persistActions.ContainsKey(message.Topic))
-            {
-                _persistActions[message.Topic].Invoke(message.Payload);
-            }
+            if (_subscriptionId == null) return;
+            _messaging.Unsubscribe(_subscriptionId);
         }
-
-        protected void WaitForValueSet(ref string value, int timeoutSeconds)
-        {
-            var c = 0;
-            while (value == null && ++c < (timeoutSeconds * 5))
-                Thread.Sleep(200);
-
-            if (value == null)
-                throw new TimeoutException();
-        }
-
-        protected void PayloadToVariable<T>(string topic, Action<string> persistAction) where T : IMessageHandler
-        {
-            _messaging.Subscribe<T>(topic);
-            _persistActions[topic] = persistAction;
-        }
-
-        protected void PayloadToVariable(string topic, Action<string> persistAction)
-        {
-            _messaging.Subscribe(topic, msg => persistAction(msg.Payload));
-        }
-
-        protected void TraceDocumentation(string title, string doc) => TraceDocumentationAction?.Invoke(title, doc);
-
-        protected void TraceTitle(string title) => TraceTitleAction?.Invoke(title);
-
-        public abstract void Run();
     }
 }
