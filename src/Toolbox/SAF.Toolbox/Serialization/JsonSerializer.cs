6f8346e1304d4666273eef4b2da6a196a5f79df5
Bernd Brachmaier | AUNOVIS <brachmaier@aunovis.de>
2023-10-19 12:54:17 +0200

    Add workaround for deserialization to 'object'

--- src/Toolbox/SAF.Toolbox/Serialization/JsonSerializer.cs
+++ src/Toolbox/SAF.Toolbox/Serialization/JsonSerializer.cs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2017-2020 TRUMPF Laser GmbH
+// SPDX-FileCopyrightText: 2017-2023 TRUMPF Laser GmbH
 //
 // SPDX-License-Identifier: MPL-2.0
 
@@ -13,44 +13,52 @@ namespace SAF.Toolbox.Serialization
 {
     public static class JsonSerializer
     {
-        private static readonly JsonSerializerOptions CamelCaseDefaultSettings = new()
-        {
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            PropertyNameCaseInsensitive = true,
-            IncludeFields = true,
-            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-            ReadCommentHandling = JsonCommentHandling.Skip,
-            AllowTrailingCommas = true,
-            WriteIndented = false,
-            UnknownTypeHandling = JsonUnknownTypeHandling.JsonNode
-        };
-
+        private static readonly JsonSerializerOptions DefaultOptions = CreateDefaultOptions();
+        
         public static string Serialize(object obj)
-            => JsonTransformer.Serialize(obj, CamelCaseDefaultSettings);
+            => JsonTransformer.Serialize(obj, DefaultOptions);
 
         public static string Serialize(object obj, params IJsonObjectConverter[] converters)
-            => JsonTransformer.Serialize(obj, DefaultSettingsWithConverters(converters));
+            => JsonTransformer.Serialize(obj, DefaultOptionsWithConverters(converters));
 
         public static T Deserialize<T>(string json)
-            => JsonTransformer.Deserialize<T>(json, CamelCaseDefaultSettings);
+            => JsonTransformer.Deserialize<T>(json, DefaultOptions);
 
         public static object Deserialize(string json, Type type)
-            => JsonTransformer.Deserialize(json, type, CamelCaseDefaultSettings);
+            => JsonTransformer.Deserialize(json, type, DefaultOptions);
 
         public static T Deserialize<T>(string json, params IJsonObjectConverter[] converters)
-            => JsonTransformer.Deserialize<T>(json, DefaultSettingsWithConverters(converters));
+            => JsonTransformer.Deserialize<T>(json, DefaultOptionsWithConverters(converters));
 
         public static object Deserialize(string json, Type type, params IJsonObjectConverter[] converters)
-            => JsonTransformer.Deserialize(json, type, DefaultSettingsWithConverters(converters));
+            => JsonTransformer.Deserialize(json, type, DefaultOptionsWithConverters(converters));
 
-        private static JsonSerializerOptions DefaultSettingsWithConverters(IJsonObjectConverter[] converters)
+        private static JsonSerializerOptions DefaultOptionsWithConverters(IJsonObjectConverter[] converters)
         {
-            var settings = new JsonSerializerOptions(CamelCaseDefaultSettings);
+            var settings = new JsonSerializerOptions(DefaultOptions);
             foreach (var c in converters.Select(c => new JsonObjectConverter(c) as JsonConverter))
             {
                 settings.Converters.Add(c);
             }
             return settings;
+        }
+
+        private static JsonSerializerOptions CreateDefaultOptions()
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                PropertyNameCaseInsensitive = true,
+                IncludeFields = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                ReadCommentHandling = JsonCommentHandling.Skip,
+                AllowTrailingCommas = true,
+                WriteIndented = false
+            };
+
+            options.Converters.Add(new ObjectToInferredTypesConverter());
+
+            return options;
         }
     }
 }
---
+++ src/Toolbox/SAF.Toolbox/Serialization/ObjectToInferredTypesConverter.cs
@@ -0,0 +1,27 @@
+﻿// SPDX-FileCopyrightText: 2017-2023 TRUMPF Laser GmbH
+//
+// SPDX-License-Identifier: MPL-2.0
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SAF.Toolbox.Serialization;
+
+internal class ObjectToInferredTypesConverter : JsonConverter<object>
+{
+    public override object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        => reader.TokenType switch
+        {
+            JsonTokenType.True => true,
+            JsonTokenType.False => false,
+            JsonTokenType.Number when reader.TryGetInt64(out var l) => l,
+            JsonTokenType.Number => reader.GetDouble(),
+            JsonTokenType.String when reader.TryGetDateTime(out var datetime) => datetime,
+            JsonTokenType.String => reader.GetString()!,
+            _ => JsonDocument.ParseValue(ref reader).RootElement.Clone()
+        };
+
+    public override void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
+        => System.Text.Json.JsonSerializer.Serialize(writer, value, value.GetType(), options);
+}
