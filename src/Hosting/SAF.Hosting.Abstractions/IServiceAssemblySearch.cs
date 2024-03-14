9134c2a7094060d7614e601b38d5e12c6e3f6417
Bernd Brachmaier | AUNOVIS <brachmaier@aunovis.de>
2024-03-14 08:37:02 +0100

    Added some doc comments

--- src/Hosting/SAF.Hosting.Abstractions/IServiceAssemblySearch.cs
+++ src/Hosting/SAF.Hosting.Abstractions/IServiceAssemblySearch.cs
@@ -4,7 +4,16 @@
 
 namespace SAF.Hosting.Abstractions;
 
+/// <summary>
+/// Interface that is used to search for service assemblies and to load their service assembly manifests.
+/// A service assembly search is used to discover service assemblies that are not directly added to the service assembly manager
+/// but are located in a specific directory that can be added by calling AddServiceAssemblySearch.
+/// </summary>
 public interface IServiceAssemblySearch
 {
+    /// <summary>
+    /// Searches for service assemblies in the configured directory and loads their service assembly manifests.
+    /// </summary>
+    /// <returns>An enumeration of the found and loaded <see cref="IServiceAssemblyManifest"/> instances.</returns>
     IEnumerable<IServiceAssemblyManifest> LoadServiceAssemblyManifests();
 }
