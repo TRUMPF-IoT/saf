b11c18145cc139e4ace2f8faee8cd3b476aa4102
Bernd Brachmaier | AUNOVIS <brachmaier@aunovis.de>
2026-07-17 17:32:03 +0200

    Refactor plugin assembly validators into contracts/extensions
    Update docs

--- src/PluginSystem/SAF.PluginSystem.Hosting.Tests/PluginSystemHostBuilderExtensionsTests.cs
+++ src/PluginSystem/SAF.PluginSystem.Hosting.Tests/PluginSystemHostBuilderExtensionsTests.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NSubstitute;
+using System.Linq;
 using System.Reflection;
 using System.IO.Abstractions;
 using Testably.Abstractions.Testing;
