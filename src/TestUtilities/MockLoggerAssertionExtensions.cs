884ee3808b7efa9d7f81d17417ce2c5a1b9f8785
Bernd Brachmaier | AUNOVIS <brachmaier@aunovis.de>
2026-09-13 17:18:37 +0200

    Solve sonar issues

--- src/TestUtilities/MockLoggerAssertionExtensions.cs
+++ src/TestUtilities/MockLoggerAssertionExtensions.cs
@@ -11,21 +11,21 @@ public static class MockLoggerAssertionExtensions
     public static void AssertLogged(this MockLogger substitute, LogLevel level)
         => substitute.Received().Log(level, Arg.Any<string>());
 
-    public static void AssertNotLogged(this MockLogger substitute, LogLevel level)
-        => substitute.DidNotReceive().Log(level, Arg.Any<string>());
-
     public static void AssertLogged<T>(this MockLogger<T> substitute, LogLevel level)
         => substitute.Received().Log(level, Arg.Any<string>());
+
+    public static void AssertLogged(this MockLogger substitute, LogLevel level, Func<string, bool> messagePredicate)
+        => substitute.Received().Log(level, Arg.Is<string>(message => messagePredicate(message!)));
+
+    public static void AssertLogged(this MockLogger substitute, Func<string, bool> messagePredicate)
+        => substitute.Received().Log(Arg.Any<LogLevel>(), Arg.Is<string>(message => messagePredicate(message!)));
 
     public static void AssertLoggedOnce(this MockLogger substitute, LogLevel level)
         => substitute.Received(1).Log(level, Arg.Any<string>());
 
-    public static void AssertLogged(this MockLogger substitute, LogLevel level, Func<string, bool> messagePredicate)
-        => substitute.Received().Log(level, Arg.Is<string>(message => messagePredicate(message)));
-
-    public static void AssertLogged(this MockLogger substitute, Func<string, bool> messagePredicate)
-        => substitute.Received().Log(Arg.Any<LogLevel>(), Arg.Is<string>(message => messagePredicate(message)));
+    public static void AssertNotLogged(this MockLogger substitute, LogLevel level)
+        => substitute.DidNotReceive().Log(level, Arg.Any<string>());
 
     public static void AssertNotLogged(this MockLogger substitute, Func<string, bool> messagePredicate)
-        => substitute.DidNotReceive().Log(Arg.Any<LogLevel>(), Arg.Is<string>(message => messagePredicate(message)));
+        => substitute.DidNotReceive().Log(Arg.Any<LogLevel>(), Arg.Is<string>(message => messagePredicate(message!)));
 }
